2bdc3774cb5efe3ee42cda09902ea5c7e04344e6
Hassan147 <79465738+Hassant147@users.noreply.github.com>
2025-04-04 17:29:51 +0500

    modified settings

--- Search/urls.py
+++ Search/urls.py
@@ -21,6 +21,7 @@ from django.urls import path, include, re_path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
+from django.views.static import serve
 
 schema_view = get_schema_view(
     openapi.Info(
